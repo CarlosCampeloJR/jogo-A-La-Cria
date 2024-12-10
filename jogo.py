9e6a83597b0da2342588c90bd55847d7f3d9ad64
Júnior Campelo <c.juniorxavier15@gmail.com>
2024-12-09 21:09:13 -0300

    tela1
    fiz a primeira tela

--- jogo.py
+++ jogo.py
@@ -1,1 +1,12 @@
                    #codigo Principal do Jogo#
+import pyxel
+
+def update():
+    pass
+
+def draw():
+    pyxel.cls(0)
+    pyxel.text(25, 45, 'Feito por :\nCarlos Eduardo Xavier Campelo Junior\nNicolas Gauterio Xavier', 7)#pyxel.frame_count % 16)
+    
+pyxel.init(180, 120, title='Hello')
+pyxel.run(update, draw)
